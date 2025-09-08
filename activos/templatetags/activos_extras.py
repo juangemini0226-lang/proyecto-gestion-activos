1a9e844f3b0184a96c63f058ecf40aa2b9338d59
Juan <juan.gemini0226@gmail.com>
2025-09-08 15:03:10 -0500

    nuevo

--- activos/templatetags/activos_extras.py
+++ activos/templatetags/activos_extras.py
@@ -8,3 +8,9 @@ def is_pdf(value):
     if not value:
         return False
     return str(value).lower().endswith('.pdf')
+
+
+@register.simple_tag
+def media_abs(request, path):
+    """Return an absolute URI for the given media path."""
+    return request.build_absolute_uri(path)
